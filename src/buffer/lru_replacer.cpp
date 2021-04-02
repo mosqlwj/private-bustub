17537dea252195caffa3ffab88cb4b49c666425d
Redemptionc <842391412@qq.com>
2021-04-02 11:03:54 +0800

    lru_replacer.cpp:一个关键的改变+增减若干comment 当前的实现中，使用了一个map来track一个frame是否在replacer中，那么在victim的时候就需要update map，来表示当前victim frame已经不在replacer中了

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -11,23 +11,34 @@
 //===----------------------------------------------------------------------===//
 
 #include "buffer/lru_replacer.h"
-
+// TODO 必须要保证是线程安全的？这里没加锁呢
 namespace bustub {
 
 LRUReplacer::LRUReplacer(size_t num_pages) { cap = num_pages; }
 
 LRUReplacer::~LRUReplacer() = default;
 
+/**
+ * 当前的实现是：如果replacer为空，那么出参为nullptr，返回false；如果不为空，那么出参为back（list中最后一个），返回true
+ * 关键的一点在于：LRU是如何体现的？现在的设计是，按照加入到replacer中的顺序，新加入的放在最前面，LRU一次也就对应最后面的
+ * 这样对吗？
+ */
 bool LRUReplacer::Victim(frame_id_t *frame_id) {
   if (frames.empty()) {
     frame_id = nullptr;
     return false;
   }
   *frame_id = frames.back();
+  cnt[*frame_id] =0;
   frames.pop_back();
   return true;
 }
 
+/**
+ * 按照specification的说法，这个函数是在BPM中pin了之后才被调用
+ * 但是这个说法很confusing，BPM没有pin操作啊，pin不是replacer的吗
+ * 不过specification后面也说了，他的一个作用是从replacer中remove对应frame
+ */
 void LRUReplacer::Pin(frame_id_t frame_id) {
   if (cnt[frame_id] == 0) {
     return;
@@ -37,6 +48,12 @@ void LRUReplacer::Pin(frame_id_t frame_id) {
   cnt[frame_id] = 0;
 }
 
+/**
+ * specification：这个函数应该在对应page的pinCnt降到0时被调用
+ * （其实LRU这个的实现很简单，唯一的不确定在于LRU的依据；另外一点，是性能现在不是最优化的）
+ * 一个作用是将frame添加到replacer
+ * @param frame_id
+ */
 void LRUReplacer::Unpin(frame_id_t frame_id) {
   // 必须要判断存不存在，不要重复插入;如果已经存在，是什么都不需要做的，见测试case1
   if (cnt[frame_id]) {
--- test/buffer/lru_replacer_test.cpp
+++ test/buffer/lru_replacer_test.cpp
@@ -21,7 +21,7 @@ namespace bustub {
 
 TEST(LRUReplacerTest, SampleTest) {
   LRUReplacer lru_replacer(7);
-  // TODO poolsize是7，但是unpin了1~6 6个frame，所以size返回6，注意size返回的是可以evict的elem的数量
+  //  poolsize是7，但是unpin了1~6 6个frame，所以size返回6，注意size返回的是可以evict的elem的数量
   // TODO 实现1：unpin的时候（实际上pin的时候也一样），首先判断在不在list里，如果在，unpin不需要做任何事
   // Scenario: unpin six elements, i.e. add them to the replacer.
   lru_replacer.Unpin(1);
@@ -33,9 +33,9 @@ TEST(LRUReplacerTest, SampleTest) {
   lru_replacer.Unpin(1);
   EXPECT_EQ(6, lru_replacer.Size());
 
-  // TODO 假设最近使用的放在最前面，那么此时是 1 6 5 4 3 2，根据LRU，victim应该是最后面的，为什么这里是1呢，应该是指，1已经在replacer里了，unpin对他是没有作用的
-  // TODO 实现1：victim，直接back加pop_back,为空就返回nullptr
-  // 那么问题来了，lru_replacer是根据什么维护LRU的性质？按照他们被放进lru_replacer的顺序？ 如果只看被放进replacer的顺序，那么此时应该是6,5,4,3,2,1
+  //  假设最近使用的放在最前面，那么此时是 1 6 5 4 3 2，根据LRU，victim应该是最后面的，为什么这里是1呢，应该是指，1已经在replacer里了，unpin对他是没有作用的
+  //  实现1：victim，直接back加pop_back,为空就返回nullptr
+  // TODO 那么问题来了，lru_replacer是根据什么维护LRU的性质？按照他们被放进lru_replacer的顺序？ 如果只看被放进replacer的顺序，那么此时应该是6,5,4,3,2,1
   // 所以victim是1，2，3没错,最后replacer中剩下6,5,4
   // Scenario: get three victims from the lru.
   int value;
@@ -46,10 +46,10 @@ TEST(LRUReplacerTest, SampleTest) {
   lru_replacer.Victim(&value);
   EXPECT_EQ(3, value);
 
-  // TODO Pin说明该page不应该被victim,是不是就是根据pin来改变lru中的顺序？当然pin(3)没有用，因为已经victim了，而pin(4)会把4放到最前面，变成4,6,5?
-  // TODO 实现1：pin是为了从replacer中移除，这里3已经不在replacer中了，所以不做任何事，4在里面，所以从里面移除
+  //  Pin说明该page不应该被victim,是不是就是根据pin来改变lru中的顺序？当然pin(3)没有用，因为已经victim了，而pin(4)会把4放到最前面，变成4,6,5?
+  //  实现1：pin是为了从replacer中移除，这里3已经不在replacer中了，所以不做任何事，4在里面，所以从里面移除
   // 但是看下面的size，变成了2，貌似意思是，一旦pin了，就从replacer中移除？不过size返回的是可以evict的数量，pin了之后本身也不可以evict了
-  // 问题在于，pin之后，他还在replacer中吗？还是说这个问题不重要？
+  // 问题在于，pin之后，他还在replacer中吗？还是说这个问题不重要？ pin了，就说了有人在使用它，肯定不能被evict，所以不在replacer中
   // Scenario: pin elements in the replacer.
   // Note that 3 has already been victimized, so pinning 3 should have no effect.
   lru_replacer.Pin(3);
@@ -57,11 +57,11 @@ TEST(LRUReplacerTest, SampleTest) {
   EXPECT_EQ(2, lru_replacer.Size());
 
   // TODO 刚pin了4，现在对4 unpin，相当于又加入replacer？ 这里说的ref bit是指什么？设为1？到底要维护pin_cnt吗？可是为什么page类
-  // // TODO 实现1：不知道这个ref bit是什么鬼 是需要维护一个ref cnt吗？多个pin需要几个unpin？（这个问题很关键）
   // 也维护了pin_cnt呢
+  // // TODO 实现1：不知道这个ref bit是什么鬼 是需要维护一个ref cnt吗？多个pin需要几个unpin？（这个问题很关键）
   // Scenario: unpin 4. We expect that the reference bit of 4 will be set to 1.
   lru_replacer.Unpin(4);
-  // TODO 现在victim又依次是5，6，4，确实符合之前说的顺序
+  //  现在victim又依次是5，6，4，确实符合之前说的顺序
   // Scenario: continue looking for victims. We expect these victims.
   lru_replacer.Victim(&value);
   EXPECT_EQ(5, value);
